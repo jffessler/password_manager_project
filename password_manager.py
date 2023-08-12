4a36059d6924ebdd5923baaed9ec73b9cd247c10
John Fessler <jffessler@gmail.com>
2023-08-12 01:30:37 -0700

    brought the manager to completion, encryption and decryption based on the use of the master password

--- password_manager.py
+++ password_manager.py
@@ -11,45 +11,58 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 #     with open("key.key", 'wb') as key_file:
 #         key_file.write(key)
 
+#read the cryptographic key from the key file, this can be set up to have multiple and rotating keys
 def read_key():
     file = open("key.key", "rb")
     key = file.read()
     file.close()
     return key
 
+#mode selection and the definition of the master key for both returning and new users
 def mode():
     master_pwd = input("State your master password: ")
-    salt = os.urandom(16)
-    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=480000,)
-    key = base64.urlsafe_b64encode(kdf.derive(read_key()+master_pwd.encode()))
 
-    # key = read_key()+master_pwd.encode()
-    fer = Fernet(key)
     while True:
         mode = input("Would you like to add a new password or view an existing password? (add/view) || Press Q to quit ").lower()
         if mode == "q":
             break
         elif mode == "add":
-            add(fer)
+            add(master_pwd)
         elif mode == "view":
-            view(fer)
+            view(master_pwd)
         else:
             print("Invalid mode!")
 
-def add(fer):
+#function for the addition of new data
+def add(master_pwd):
+    #defining the key tied to each password
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=480000,)
+    key = base64.urlsafe_b64encode(kdf.derive(read_key()+master_pwd.encode()))
+    fer = Fernet(key)
+
+    #account and password input and defining the data to be stored for subsequent retrieval and decryption
     account_name = input("Account Name: ")
     pwd = input("New Password: ")
     token = fer.encrypt(pwd.encode())
     with open("passwords.txt", "a") as f:
-        f.write(account_name + " | " + str(token) + "\n")
-    return token
+        f.write(account_name + " | " + token.decode() + " | " + key.decode() + " | " + master_pwd + "\n")
+    print(fer.decrypt(token))
 
-def view(fer):
+#function for the viewing of stored data
+def view(master_pwd):
     with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
-            user, passw = data.split(' | ')
-            print(f"User: {user} | Password: {(fer.decrypt(fer.encrypt(passw.encode()))).decode()}")
+            user, passw, key, master_pwd_o = data.split(' | ')
+            f = Fernet(key)
+
+            #decrypting based on the input of the master key
+            if master_pwd == master_pwd_o:
+                decoded = f.decrypt(passw.encode())
+                print(f"Account: {user} | Password: {decoded}")
+            else:
+                print("You don't have access to that password")
 
 def run_manager():
     mode()
